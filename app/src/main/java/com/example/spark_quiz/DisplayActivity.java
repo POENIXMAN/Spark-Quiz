8c6d7b60d83e0c82931fb2b27a565b5b7f373d73
POENIXMAN <userr100p@gmail.com>
2023-12-12 12:46:05 +0200

    main menu and restart buttons work

--- app/src/main/java/com/example/spark_quiz/DisplayActivity.java
+++ app/src/main/java/com/example/spark_quiz/DisplayActivity.java
@@ -2,7 +2,11 @@ package com.example.spark_quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class DisplayActivity extends AppCompatActivity {
 
@@ -15,6 +19,24 @@ public class DisplayActivity extends AppCompatActivity {
         application = (SApp) getApplication();
         ScoreChart sc = findViewById(R.id.scorechart);
         sc.setScore(application.getScore());
+
+        Button main_menu = findViewById(R.id.menu_btn);
+        main_menu.setOnClickListener(v-> {
+            Intent intent = new Intent(DisplayActivity.this, HomeActivity.class);
+            application.setScore(new int[4]);
+            application.setAnswers(new byte[20]);
+            application.setFetched(false);
+            application.setQuestions(new ArrayList<>());
+            startActivity(intent);
+        }
+        );
+
+        Button try_again = findViewById(R.id.again_btn);
+        try_again.setOnClickListener(v->{
+            Intent intent = new Intent(DisplayActivity.this, MainActivity.class);
+            application.setAnswers(new byte[20]);
+            startActivity(intent);
+        });
 
 
     }
